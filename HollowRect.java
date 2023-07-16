8dc67cbd74cfcdb82a4055502f55d2168833aa06
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-07-16 13:00:48 +0530

    Update HollowRect.java

--- HollowRect.java
+++ HollowRect.java
@@ -17,3 +17,26 @@ o/p:
 *    *
 *    * 
 *****  
+
+/*2)print inverted traingle
+  expexted output:
+      *
+     * *
+   * * *
+*  * * *
+ */
+
+  class Inverted_trangle{
+    public static void main(String[]args){
+        int n=3;
+    for(int i=0;i<n;i++){
+        /*spaces*/for(int j=0;j<n-i;j++){
+            System.out.println("");
+        }
+         /*star*/for(int j=0;j<i;j++){
+            System.out.println("*");
+        }
+        System.out.println();
+        }
+    }
+}
