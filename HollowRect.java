b82d34c55a742b917329d310619a0e4a400c92df
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-07-16 11:57:35 +0530

    Create HollowRect.java
    Print Holllow rectangle pattern..
    o/p:
    *****
    *    *
    *    * 
    *****

---
+++ HollowRect.java
@@ -0,0 +1,19 @@
+import java.util.*;
+void prinHolloRect(int rowLen,int colLen,int i,int j)
+  {
+  for(int i=0;i<=rowLen;i++){
+    for(int i=0;i<=colLen;i++){
+      if(i==1||i==rowlen||j==1||j==colLen){
+        System.out.println("*");
+      }
+      System.out.println("*");
+    }
+    
+  }
+  }
+
+o/p:
+*****
+*    *
+*    * 
+*****  
